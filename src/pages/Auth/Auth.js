90dcc600b01a345de26d043e383728eb98414309
opnways <admin@opnways.com>
2022-08-26 14:32:50 +0200

    Login Register Routes

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './Auth.scss'
-
+import { Button } from 'semantic-ui-react';
+import { AuthOptions, RegisterForm, LoginForm } from '../../components';
 export  function Auth() {
+  const [ typeForm, setTypeForm ] = React.useState(null);
+  const openLoginForm = () => setTypeForm("login")
+  const openRegisterForm = () => setTypeForm("register")
+  const goBack = () => setTypeForm(null)
+
+ const renderForm = () => {
+  if(typeForm === 'login'){
+    return <LoginForm openRegisterForm={openRegisterForm} goBack={goBack}/>
+  }
+  if(typeForm === 'register'){
+    return <RegisterForm openLoginForm={openLoginForm} goBack={goBack}/>
+  }
+  return <AuthOptions openLoginForm={openLoginForm} openRegisterForm={openRegisterForm}/>
+}
+  
   return (
-    <div><button  class="ui primary button"> Auth Screen</button></div>
+    <div>
+    {renderForm()}</div>
   )
 }
