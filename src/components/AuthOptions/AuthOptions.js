ae66b8681ec75de9bf80e29719a8bd2f73674780
opnways <admin@opnways.com>
2022-08-28 16:47:44 +0200

    SCSS AuthOptions

--- src/components/AuthOptions/AuthOptions.js
+++ src/components/AuthOptions/AuthOptions.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Button} from 'semantic-ui-react';
-
+import "./AuthOptions.scss";
 export  function AuthOptions(props) {
   const {openLoginForm, openRegisterForm} = props;
   return (
-    <div style={{backgroundColor: "#000"}}>
-        <h1>AuthOptions</h1>
-        <Button onClick={openRegisterForm} primary>Registro</Button>
-        <Button onClick={openLoginForm} secondary>Login</Button>
+    <div className='auth-options'>
+        <h1>Tu aplicación de podcast gratuita</h1>
+        <Button onClick={openRegisterForm} className="register">Registrate gratis</Button>
+        <Button onClick={openLoginForm} className="login">Inicia sesión</Button>
     </div>
   )
 }
